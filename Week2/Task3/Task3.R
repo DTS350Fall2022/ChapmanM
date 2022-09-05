5a7cc9703e5f87ad1a0842fd640ffbeea154e51e
matthewchapman <mjchapmanwjc@gmail.com>
2022-09-05 18:27:55 -0500

    Task 3

--- Week2/Task3/Task3.R
+++ Week2/Task3/Task3.R
@@ -13,3 +13,12 @@ ggplot(data = iris, mapping = aes(x = Petal.Length, y = Petal.Width)) +
 ggplot(data = iris, mapping= aes(x=Sepal.Length)) + 
   geom_histogram(mapping = aes(fill = Species), color = "black",bins=18) +
   geom_vline(mapping = aes(xintercept = mean(Sepal.Length)), linetype = "dashed", color = "grey")
+
+
+#Driving Question:
+#Which species has the most unique (variety of sizing) petals?
+
+#Answer:
+#The third visualization, the scatter plot with a linear line, shows that 
+#virginicas have the most outliers when it comes to peteal width and length.
+#The other species stay much closer to the line.
